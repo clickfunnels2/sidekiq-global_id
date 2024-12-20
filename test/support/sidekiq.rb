6fb5382ffa366fcf535b0daf9b32c39be8e7a1ff
Nate Vick <nate.vick@hint.io>
2024-12-19 16:51:22 -0800

    Fix tests to be compatible with Sidekiq 7

--- test/support/sidekiq.rb
+++ test/support/sidekiq.rb
@@ -3,12 +3,16 @@
 require "sidekiq"
 require "sidekiq/testing"
 
-Sidekiq.client_middleware do |chain|
-  chain.add Sidekiq::GlobalID::ClientMiddleware
+Sidekiq.configure_client do |config|
+  config.client_middleware do |chain|
+    chain.add Sidekiq::GlobalID::ClientMiddleware
+  end
 end
 
-Sidekiq.server_middleware do |chain|
-  chain.add Sidekiq::GlobalID::ServerMiddleware
+Sidekiq.configure_server do |config|
+  config.server_middleware do |chain|
+    chain.add Sidekiq::GlobalID::ServerMiddleware
+  end
 end
 
 Sidekiq::Testing.fake!
